9e051a49afef168c99bc43a4639a6dd565506f9d
thefalked <giulainocrivelli@hotmail.com>
2021-03-20 15:15:05 -0400

    adding Intl for formating date and currency

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -46,13 +46,18 @@ export function TransactionsTable() {
                 {transaction.title}
               </td>
               <td className={transaction.type}>
-                {transaction.amount}
+                {new Intl.NumberFormat('pt-BR', {
+                  style: "currency",
+                  currency: "BRL"
+                }).format(transaction.amount)}
               </td>
               <td>
                 {transaction.category}
               </td>
               <td>
-                {transaction.createdAt}
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
               </td>
             </tr>
           ))}
